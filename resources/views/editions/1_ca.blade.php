45375546fb5f3a362b6aeececa11c2e47c97e95b
Guillem <guillem@disedit.com>
2018-07-31 00:38:28 +0200

    Add About page

---
+++ resources/views/editions/1_ca.blade.php
@@ -0,0 +1,45 @@
+<div class="about-block">
+    <h3>Altea imagina, Altea participa</h3>
+    <img src="/images/choice-2.svg" class="about-block__thumbnail" />
+    <p>L’<strong>Ajuntament d’Altea</strong> ha establert la regeneració democrática com uns dels seus pilars bàsics. Una regeneració que passa per donar veu als ciutadans i ciutadanes i apropar-los a la pressa de decisions. Una participació que té per finalitat que la ciutania puga assolir majors quotes de corresponsabilitat política, que <strong>“co-governen” el seu ajuntament, el seu poble</strong>.
+    <p>Ja ho vam començar l’any passat amb els primers pressupostos participatius i en aquesta segona edició, el procés de participació ciutadana será molt més ampli i fácil.</p>
+    <p><strong>Altea Participa</strong> pretén aprofundir en la participació real i efectiva de la ciutadania.</p>
+</div>
+
+<div class="about-block">
+<h3>La ciutadania decideix</h3>
+<p>Els veïns i veïnes, associacions… han de de fer les seues propostes i enviar-les utilitzant el formulari que està penjat a la web <a href="https://www.alteaparticipa.es">www.alteaparticipa.es</a> i enviar-lo per correu electrónic a <a href="mailto:participacio@altea.es">participacio@altea.es</a> o bé, entregar-lo per registre d’entrada a l’<strong>Oficina d’Atenció al Ciutadà (OAC)</strong> que està ubicat a la planta Baixa de l’<strong>Ajuntament d’Altea</strong> o també a l’<strong>Extensió Administrativa d’Altea la Vella</strong>.  El termini per presentar les propostes finaliza el <strong>30 de setembre</strong>.</p>
+<p>Les iniciatives / propostes / projectes que es presenten hauran de correspondre’s amb inversions municipals i amb la finalitat de fer d’<strong>Altea</strong> un poble millor per a totes i tots.</p>
+<p>Totes i cadascuna de les propostes seran avaluades pels serveis tècnics de l’Ajuntament per tal de comprobar que, de manera estimativa, no superen els límits pressupostaris, que s’ajusten a la viabilitat i que son competències de l’Ajuntament i no del Ministeri o de Conselleria.</p>
+<p>Posteriorment, será el <strong>Consell de Participació Ciutadana</strong> on estan representants tots els Consells Sectorials d’Altea quí decidirá finalment quins <strong>10 projectes</strong> eixiran a votació.</p>
+<p>Prèviament a la votació, s’explicarà un per un tots els projectes que finalment eixiran a votació per tal que la ciutania tinga <strong>tota la informació al seu abast</strong>. Les 10 propostes finalistes, seran explicades per la persona o entitat que l’haja presentada.</p>
+<p>Finalment, seran els ciutadans i ciutadanes els que es pronuncien sobre <strong>quins seran els 3 projectes</strong> que consideren més oportuns que l’Ajuntament d’Altea realitze.</p>
+<p>Tota la informació al voltant dels projectes / propostes es podrá consultar a la web www.alteaarticipa.es.</p>
+<p>La ciutadania decidirá al voltant dels projectes que proposen els veïns.</p>
+</div>
+
+<div class="about-block">
+<h3>Què anem a decidir?</h3>
+<p class="about-block__highlight"><i class="far fa-question-circle"></i> En aquest procés de participación ciutadana es s’ha de votar les <strong>tres propostes que es consideren millor per al poble d’Altea</strong> amb una inversión máxima de <strong>100.00€</strong>.</p>
+</div>
+
+<div class="about-block">
+    <h3>Informació sobre la votació</h3>
+    <img src="/images/choice.svg" class="about-block__outer-thumbnail" />
+    <div class="columns">
+        <h4>Qui pot participar a Altea Participa?</h4>
+        <p>Podran participar en aquest procés aquelles persones <strong>majors de 16 anys empadronades a Altea</strong>, d’acord al padró de data <strong>01/06/2018</strong>.</p>
+
+        <h4>Què es votarà?</h4>
+        <p>Es podran votar <strong>3 de les 10 propostes</strong> elegides prèviament pel <strong>Consell de Participació Ciutadana</strong> d’entre les presentades pels ciutadans. La decisió que manifeste un ciutadà o ciutadana esdevindrà ineficaç si s’escullen més de 3 projectes.</p>
+
+        <h4>Es podrà votar per internet?</h4>
+        <p>Es podrà votar mitjançant una plataforma d’internet i amb un sistema de <strong>votació telemàtica</strong> accessible des de qualsevol ordinador o dispositiu mòbil. Per cada línea IP es podran efectuar 3 votacions. A aquesta web, s’haurà d’introduir prèviament el DNI a la casella que s’indique. Aquelles persones que descobrisquen que algú ha suplantat la seua identitat o vullguen rectificar el seu vot, podran dirigir-se a qualsevol dels punts de votació ubicats als edificis municipals i demanar a la persona encarregada l’anul·lació del vot emès i la voluntat de tornar a votar, presentant el seu DNI.</p>
+
+        <h4>Inci i final de la votació</h4>
+        <p>(en uns dies ho tindrem ja clar)</p>
+
+        <h4>Resultat de la votació</h4>
+        <p>El resultat de la votació es farà públic a aquesta web només concloga el període de votació.</p>
+    </div>
+</div>
