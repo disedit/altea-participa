9280d083929edfe6c5082f8cdb397af8b53674e2
Guillem <guillem@disedit.com>
2018-08-13 00:05:55 +0200

    Add SVG divider on header

--- resources/views/components/voteinfo.blade.php
+++ resources/views/components/voteinfo.blade.php
@@ -37,5 +37,12 @@
                 @endif
             </div>
         </div>
+        @if(!$compact)
+            <div class="vote-info__divider">
+                <svg version="1.1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1366 52" preserveAspectRatio="none">
+                    <path fill="white" d="M0,0c167.8,5.2,330.8,14.8,493.6,25.4c60.7,3.9,132.5,8.7,179,10.4c366.4,13.7,693.9-22.1,693.5-35 c0-0.1,0-0.2-0.1-0.3L1366,52H0V0z"/>
+                </svg>
+            </div>
+        @endif
     @endif
 </header>
