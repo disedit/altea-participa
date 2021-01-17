943d0795a7566680b86d5b71666ff30ecc577d0b
guillem <guillem@disedit.com>
2021-01-17 17:33:52 +0100

    Add page-header blade section

--- resources/views/components/voteinfo.blade.php
+++ resources/views/components/voteinfo.blade.php
@@ -1,26 +1,31 @@
 @php
   $compact = (!Request::segment(1)) ? false : true;
 @endphp
+
 <header class="vote-info {{ (!$compact) ? 'vote-info--full' : 'vote-info--compact' }}" role="banner">
     @if(!$inPerson)
         <div class="container">
             <div class="row">
                 <div class="{{ ($compact) ? 'col-lg-8' : 'col-lg-6' }} vote-info__text">
-                    <h2><span>@lang('participa.heading')</span></h2>
-                    <p class="vote-info__intro"><span>@lang('participa.subheading')</span></p>
+                    @hasSection('page-header')
+                        @yield('page-header')
+                    @else
+                        <h2><span>@lang('participa.heading')</span></h2>
+                        <p class="vote-info__intro"><span>@lang('participa.subheading')</span></p>
 
-                    @if(isset($edition))
-                        <p class="vote-info__action">
-                            @if($edition->isOpen())
-                                <a data-scroll href="#content" class="big-button"><i aria-hidden="true" class="fa fa-bullhorn"></i> @lang('participa.vote')</a>
-                            @elseif($edition->inProposalPhase())
-                                <a href="{{ secure_url('propose') }}" class="big-button"><i aria-hidden="true" class="fa fa-comment"></i> @lang('participa.propose_cta')</a>
-                            @elseif($edition->isPending())
-                                <a data-scroll href="#content" class="big-button"><i aria-hidden="true" class="fa fa-info-circle"></i> @lang('participa.more_info')</a>
-                            @elseif($edition->resultsPublished())
-                                <a data-scroll href="#content" class="big-button"><i aria-hidden="true" class="fa fa-chart-bar"></i> @lang('participa.results')</a>
-                            @endif
-                        </p>
+                        @if(isset($edition))
+                            <p class="vote-info__action">
+                                @if($edition->isOpen())
+                                    <a data-scroll href="#content" class="big-button"><i aria-hidden="true" class="fa fa-bullhorn"></i> @lang('participa.vote')</a>
+                                @elseif($edition->inProposalPhase())
+                                    <a href="{{ secure_url('propose') }}" class="big-button"><i aria-hidden="true" class="fa fa-comment"></i> @lang('participa.propose_cta')</a>
+                                @elseif($edition->isPending())
+                                    <a data-scroll href="#content" class="big-button"><i aria-hidden="true" class="fa fa-info-circle"></i> @lang('participa.more_info')</a>
+                                @elseif($edition->resultsPublished())
+                                    <a data-scroll href="#content" class="big-button"><i aria-hidden="true" class="fa fa-chart-bar"></i> @lang('participa.results')</a>
+                                @endif
+                            </p>
+                        @endif
                     @endif
                 </div>
 
