7be5b6d4558cccd461801b200f5dd9ff33fa4c42
guillem <guillem@disedit.com>
2022-05-04 17:56:00 +0200

    Add styles to pages

--- resources/assets/js/components/pages/Page.vue
+++ resources/assets/js/components/pages/Page.vue
@@ -1,5 +1,5 @@
 <template>
-  <div v-if="!is404">
+  <div v-if="!is404" class="page">
     <div v-if="!pending">
       <vue-markdown>{{ content }}</vue-markdown>
     </div>
@@ -43,26 +43,108 @@ export default {
 
   computed: {
     content () {
-      return this.page ? this.page[0].contingut_va : ''
+      return this.getMultilangField('contingut')
     },
 
     title () {
-      return this.page ? this.page[0].titol_va : ''
+      return this.getMultilangField('titol')
+    },
+  },
+
+  methods: {
+    getMultilangField (name) {
+      if (!this.page[0]) return
+      const page = this.page[0]
+      const { locale } = GlobalConfig
+      const fields = {
+        ca: `${name}_va`,
+        es: `${name}_es`,
+        en: `${name}_en`
+      }
+
+      return page[fields[locale]] || page[name]
     }
   }
 }
 </script>
 
-<style lang="scss" scoped>
-.loading {
-  color: #ccc;
-  font-size: 2rem;
-  text-align: center;
-  padding: 2rem 0;
-}
+<style lang="scss">
+.page {
+  max-width: 800px;
 
-.main-text {
-  font-size: 1.5rem;
-  padding: 2rem 0;
+  .loading {
+    color: #ccc;
+    font-size: 2rem;
+    text-align: center;
+    padding: 2rem 0;
+  }
+
+  .main-text {
+    font-size: 1.5rem;
+    padding: 2rem 0;
+  }
+
+  .main-list {
+    ul {
+      display: grid;
+      list-style: none;
+      padding: 0;
+      margin: 0;
+      grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
+      gap: 2rem;
+
+      li {
+        display: flex;
+      }
+
+      a {
+        flex-grow: 1;
+        display: flex;
+        align-items: flex-end;
+        border: 2px var(--primary) solid;
+        border-radius: .5rem;
+        padding: 4rem 1rem 1rem 1rem;
+        line-height: 1.1;
+        font-weight: bold;
+        font-size: 2rem;
+
+        &:hover {
+          background: var(--primary);
+          color: #fff;
+          text-decoration: none;
+        }
+      }
+    }
+  }
+
+  figure {
+    background: #f3f2ff;
+    padding: 1rem;
+    border-radius: .75rem;
+
+    img {
+      width: 100%;
+      max-height: 300px;
+      object-fit: contain;
+    }
+
+    figcaption {
+      text-align: center;
+      color: var(--primary);
+      font-size: .9rem;
+      width: 80%;
+      margin: 0 auto;
+    }
+  }
+
+  h2 {
+    font-weight: 900;
+  }
+
+  h3 {
+    font-weight: bold;
+    margin-top: 2rem;
+    font-size: 1rem;
+  }
 }
 </style>
