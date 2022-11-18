23eabcdee4ee3be5307685b089b04f2f21019ece
guillem <guillem@disedit.com>
2022-11-18 23:18:25 +0100

    Fix jquery dependency issue

--- resources/views/layouts/public.blade.php
+++ resources/views/layouts/public.blade.php
@@ -61,6 +61,7 @@
         </div>
     @show
 
+    <script src="https://code.jquery.com/jquery-3.6.1.slim.min.js" integrity="sha256-w8CvhFs7iHNVUtnSP0YKEg00p9Ih13rlL9zGqvLdePA=" crossorigin="anonymous"></script>
     <script src="{{ mix('js/common.js') }}"></script>
     @stack('scripts')
 </body>
